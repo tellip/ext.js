6f3bf09e8a8f82e699cec9437584b6c870a20c90
Tephi Luo <luojunyi.0@gmail.com>
2021-09-15 10:42:15 +0800

    asdf

--- index.js
+++ index.js
@@ -8,13 +8,13 @@
 //         ...rest
 //     );
 // };
-Reflect.map = (obj, cb) => Object.values(obj).reduce((acc, [key,value]) => {
+Reflect.map = (obj, cb) => Object.entries(obj).reduce((acc, [key,value]) => {
     acc[key] = cb(value, key, obj);
     return acc;
 }, {});
 // Reflect.every = (obj, cb) => Reflect.ownKeys(obj).every(key => cb(obj[key], key, obj));
 // Reflect.some = (obj, cb) => Reflect.ownKeys(obj).some(key => cb(obj[key], key, obj));
-Reflect.filter = (obj, cb) => Object.values(obj).filter(([key,value]) => cb(value, key, obj)).reduce((acc, [key,value]) => {
+Reflect.filter = (obj, cb) => Object.entries(obj).filter(([key,value]) => cb(value, key, obj)).reduce((acc, [key,value]) => {
     acc[key] = value;
     return acc;
 }, {});
